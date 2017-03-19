ed90da13acbd9664c8a27db422c55f2950268668
Daniel Ng <dvng13@gmail.com>
2017-03-19 14:53:14 -0700

    ConstrainedBacktracking - extend Backtracking class and import board constants

--- src/models/constrainedBacktracking.js
+++ src/models/constrainedBacktracking.js
@@ -1,6 +1,6 @@
-import SudokuSolver from './sudokuSolver'
+import Backtracking from './backtracking'
 
-export default class ConstrainedBacktracking extends SudokuSolver {
+import { EASY_BOARD_1, EASY_BOARD_2, EASY_BOARD_3 } from '../utilities/boards.js'
 
   // parse the board
   _parseInputBoard(inputBoard) {
@@ -9,6 +9,7 @@ export default class ConstrainedBacktracking extends SudokuSolver {
       let square = this.SQUARES[index];
       squares_to_values.set(square, inputSquare === "0" ? this.DIGITS : inputSquare)
     });
+export default class ConstrainedBacktracking extends Backtracking {
 
     return squares_to_values;
   }
