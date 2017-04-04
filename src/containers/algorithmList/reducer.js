086038decbcc68c35e9fa8f89df2332f7b965031
Daniel Ng <dvng13@gmail.com>
2017-04-03 20:50:16 -0700

    select backtracking algorithm by default

--- src/containers/algorithmList/reducer.js
+++ src/containers/algorithmList/reducer.js
@@ -1,4 +1,6 @@
-export default function(state = null, action) {
+import { ALGORITHM_NAMES } from '../../utilities/constants';
+
+export default function(state = ALGORITHM_NAMES[0], action) {
 
   switch(action.type) {
     case 'ALGORITHM_SELECTED':
