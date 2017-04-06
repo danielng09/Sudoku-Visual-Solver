41337589a68421c632afaa57e6d4e59c983a8c7f
Daniel Ng <dvng13@gmail.com>
2017-04-05 23:50:56 -0700

    color the default set squares of the board

--- src/components/square/view.js
+++ src/components/square/view.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import classNames from "classnames";
+
+import { DEFAULT_SQUARES } from "../../utilities/algorithmConstants";
 
 require("./style.scss");
 
@@ -8,8 +10,9 @@ export default class Square extends Component {
     let val = this.props.value;
 
     let squareClasses = classNames('square', {
-      'bottom-border': ["C", "F"].includes(this.props.row),
-      'right-border': ["3", "6"].includes(this.props.col)
+      "bottom-border": ["C", "F"].includes(this.props.row),
+      "right-border": ["3", "6"].includes(this.props.col),
+      "light-orange-text": DEFAULT_SQUARES.has(`${this.props.row}${this.props.col}`)
     });
 
     return (
--- src/utilities/algorithmConstants.js
+++ src/utilities/algorithmConstants.js
@@ -2,6 +2,10 @@ import { SudokuSolver } from '../models/algorithms';
 
 let sudokuSolver = new SudokuSolver();
 let defaultSquaresToValues = sudokuSolver.parseInputBoard();
+let defaultSquares = Array.from(defaultSquaresToValues.keys()).filter(square => {
+  return defaultSquaresToValues.get(square).length === 1;
+})
 
 export const DEFAULT_SQUARES_TO_VALUES = defaultSquaresToValues;
+export const DEFAULT_SQUARES = new Set(defaultSquares);
 export const SELECTABLE_ALGORITHM_NAMES = Object.freeze(["Backtracking", "ConstrainedBacktracking"]);
